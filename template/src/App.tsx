10f43f8ff18dc7a879ff6a26284347ac7351956b
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2022-02-03 11:20:02 +0100

    refactor(app): update eslint

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -3,13 +3,11 @@ import { ThemeProvider } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from '@pagopa/mui-italia/theme';
 
-const App = () => {
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      Template
-    </ThemeProvider>
-  );
-};
+const App: React.FC = () => (
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    Template
+  </ThemeProvider>
+);
 
 export default App;
